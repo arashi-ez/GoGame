5f7a198d66fc33f60596756805acbc25d69f44f2
Araashi <122167794+arashi-ez@users.noreply.github.com>
2023-02-25 22:05:32 +0500

    Added functions with future appling

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
+/*
+main idea of the game is to make a different probabilities and possibilities
+by the changing in that way the game always will be different
+*/
+
+//next change is to add functions which will applied randomly
+//eg. first time player should get 1 way of situation but in the next circle of game
+//player could take different story
+
 package main
 
 import (
@@ -9,25 +18,27 @@ import (
 var name string
 var age int
 
-// func _empty() this func will show some extra data
+// this func will ask a namer of the hero and initial data
+func namer() (int, string) {
 
-// this func will save all possible variant of actions
-func possibilities(int) {
-	
+	fmt.Println("What is your name?")
+	_, err := fmt.Scanln(&name)
+	if err != nil {
+		return 0, ""
+	}
+	fmt.Println("How old are you?")
+	_, err2 := fmt.Scanln(&age)
+	if err2 != nil {
+		return 0, ""
+	}
+	return age, name
 }
 
-//this func will make a random number generator
+// this func will make a random number generator
 func randomizer() int {
 
-	var seeding int
-
 	fmt.Println("Start generate your life!")
-	fmt.Println("Here you need to set the borders of your random number!")
 
-	_, err := fmt.Scanln(&seeding)
-	if err != nil {
-		return 0
-	}
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 
@@ -36,24 +47,34 @@ func randomizer() int {
 	return randee
 }
 
-func namer() { //this func will ask a namer of the hero and initial data
+// this func will save all possible variant of actions
+func possibilities(a int, b int, s string) {
+	if a < 10 && a >= 0 {
+		fmt.Printf("%s is a %d y.o king of UK", s, b)
 
-	fmt.Println("What is your name?")
-	_, err := fmt.Scanln(&name)
-	if err != nil {
-		return
+	} else if a < 20 && a >= 10 {
+		fmt.Printf("%s is a %d y.o streamer with 1b subs in YouTube", s, b)
+
+	} else if a < 30 && a >= 20 {
+		fmt.Printf("%s is a %d y.o average teenager who secretly loves his stepmom", s, b)
+
+	} else if a < 40 && a >= 30 {
+		fmt.Printf("%s is a %d y.o stepmom of an average teenager", s, b)
+
+	} else if a < 50 && a >= 40 {
+		fmt.Printf("%s is a %d y.o second-time-merried porn-producer ", s, b)
+
+	} else {
+		fmt.Printf("%s is a %d y.o GOAT of every sport all over the world", s, b)
+
 	}
-	fmt.Println("How old are you?")
-	_, err2 := fmt.Scanln(&age)
-	if err2 != nil {
-		return
-	}
-	fmt.Printf("Hi, %s, you are %d!", name, age)
 }
 
+// func _empty() this func will show some extra data
+
+// Here main code where we will apply some actions in the story game
 func main() {
-	//Here main code where we will apply some actions in the story game
 	namer()
 	randee := randomizer()
-	possibilities(randee)
+	possibilities(randee, age, name)
 }
